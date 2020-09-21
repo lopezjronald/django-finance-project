4593532e6bf62819dc3b18fb071b69f39f2c8f34
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-09-21 00:09:50 -0700

    completing about html layout

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, CareerPageView, AboutPageView, ContactPageView
 
 urlpatterns = [
+    path('career/', CareerPageView.as_view(), name='career'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
     path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class CareerPageView(TemplateView):
+    template_name = 'careers.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
