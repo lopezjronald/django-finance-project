4862aa36269272d9585fa80bb3bc9f590494925c
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-09-22 18:47:06 -0700

    modified pk for encryption

--- jobs/models.py
+++ jobs/models.py
@@ -1,9 +1,14 @@
+import uuid
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
 
 class Job(models.Model):
+    id = models.UUIDField(  # new
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
     title = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     description = models.TextField()
@@ -18,6 +23,9 @@ class Job(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('job_detail', args=[str(self.id)])
 
 # class Applicant(models.Model):
 #     job = models.ForeignKey(
--- jobs/urls.py
+++ jobs/urls.py
@@ -3,5 +3,5 @@ from .views import JobListView, JobDetailView
 
 urlpatterns = [
     path('', JobListView.as_view(), name='job_list'),
-    path('<int:pk>/', JobDetailView.as_view(), name='job_detail'),
+    path('<uuid:pk>/', JobDetailView.as_view(), name='job_detail'),
 ]
