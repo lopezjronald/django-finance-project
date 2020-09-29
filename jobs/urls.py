5bc140ab4ee4a030a246a9313ca7e869e1039515
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-09-28 20:52:14 -0700

    added .gitignore file

--- jobs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import JobListView, JobDetailView
-
-urlpatterns = [
-    path('', JobListView.as_view(), name='job_list'),
-    path('<uuid:pk>/', JobDetailView.as_view(), name='job_detail'),
-]
